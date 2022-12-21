b1071bb3eac402ff6b7fe8668765a666eab5e29b
rob tillaart <rob.tillaart@gmail.com>
2022-12-21 20:13:50 +0100

    fix examples

--- examples/Voltmeter_demo_millivolts/Voltmeter_demo_millivolts.ino
+++ examples/Voltmeter_demo_millivolts/Voltmeter_demo_millivolts.ino
@@ -16,8 +16,8 @@ void setup()
   Serial.begin(115200);
   while (!Serial);
   Serial.println(__FILE__);
-  Serial.print("VOLTMETER_LIB_VERSION: ");
-  Serial.println(VOLTMETER_LIB_VERSION);
+  Serial.print("DMM_LIB_VERSION: ");
+  Serial.println(DMM_LIB_VERSION);
 
   dmm.begin(A0, 5, 1023);
 }
